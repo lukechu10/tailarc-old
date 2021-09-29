f94009695e7a38e339f170f8a918e5d6a1493e9d
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-09-28 21:25:37 -0700

    Add message when monsters die

--- crates/tailarc/src/systems/damage.rs
+++ crates/tailarc/src/systems/damage.rs
@@ -1,6 +1,6 @@
 use bevy_ecs::prelude::*;
 
-use crate::components::{CombatStats, Player, SufferDamage};
+use crate::components::{CombatStats, EntityName, Player, SufferDamage};
 use crate::gamelog::GameLog;
 
 pub fn damage_system(
@@ -18,13 +18,20 @@ pub fn damage_system(
 pub fn delete_the_dead(
     mut commands: Commands,
     game_log: Res<GameLog>,
-    q: Query<(Entity, &CombatStats, Option<&Player>)>,
+    q: Query<(Entity, &CombatStats, Option<&EntityName>, Option<&Player>)>,
 ) {
-    for (entity, stats, player) in q.iter() {
+    for (entity, stats, name, player) in q.iter() {
         if stats.hp <= 0 {
             if player.is_some() {
+                // Player died.
                 game_log.add_entry("You died! :(".to_string());
             } else {
+                // A monster died.
+                if let Some(name) = name {
+                    game_log.add_entry(format!("{} is dead", name.name));
+                } else {
+                    // Silent death...
+                }
                 commands.entity(entity).despawn();
             }
         }
